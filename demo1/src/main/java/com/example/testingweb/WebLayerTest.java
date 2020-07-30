5b77d0af88a53be54162dc8dcfb949d937ab5136
libbysirak <64847769+libbysirak@users.noreply.github.com>
2020-07-30 14:19:24 -0400

    Add files via upload

---
+++ demo1/src/main/java/com/example/testingweb/WebLayerTest.java
@@ -0,0 +1,14 @@
+import org.springframework.beans.factory.annotation.Autowired;
+
+@WebMvcTest
+public class WebLayerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
+	}
+}
